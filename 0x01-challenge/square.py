83e0299b311a267429f133de3cf78d2bd5d66d7f
ntsakozinhle <ntsakosambo1@gmail.com>
2024-05-15 19:03:03 -0700

    My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
+""" This moduled describes square class """
+
 
 class square():
+    """ A class representing a square """
     
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, height=0):
+        """
+        Initializes the square with given width and height.
 
-    def area_of_my_square(self):
+        Args:
+            width (int): The width of the square.
+            height (int): the height of the square.
+        """
+        self.width = width
+        self.height = height
+
+    def area(self):
         """ Area of the square """
         return self.width * self.width
 
-    def permiter_of_my_square(self):
+    def permiter(self):
         """ Perimeter of the square """
-        return 4 * self.width
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """ Returns a string representation of the Square """
+        return "Square with width {} and height {}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print("Area:"s.area())
+    print("Perimeter:"s.perimeter())
